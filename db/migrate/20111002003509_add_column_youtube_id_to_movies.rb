c1ac7d2d118025a04b5c021b33995baf54f26b43
Noppanit Charassinvichai <noppanit.c@gmail.com>
2011-10-02 01:54:06 +0100

    Add youtube src

---
+++ db/migrate/20111002003509_add_column_youtube_id_to_movies.rb
@@ -0,0 +1,5 @@
+class AddColumnYoutubeIdToMovies < ActiveRecord::Migration
+  def change
+      add_column :movies, :youtube_id, :string 
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111002000220) do
+ActiveRecord::Schema.define(:version => 20111002003509) do
 
   create_table "movies", :force => true do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20111002000220) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image_src"
+    t.string   "youtube_id"
   end
 
 end
